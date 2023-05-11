ce27466b75fcbf70b6449cf9fb7ca2e5b66fbcdb
Udbhav Pangotra <udbhavpangotra@ymail.com>
2023-05-10 23:03:13 -0400

    Adding basic charts using altair

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import urllib.request
 import streamlit as st
 import altair as alt
 
+
 # Load the JSON object
 url = "https://www.reddit.com/r/AskMen/new/.json"
 response = urllib.request.urlopen(url)
 data = json.load(response)
+print("Script is running 0")
 
 # Use json_normalize to create a DataFrame from the JSON data
 df = pd.json_normalize(data, record_path=["data", "children"])
@@ -20,29 +22,18 @@ st.title("AskMen Top Posts")
 st.write("This app showcases the top posts on AskMen.")
 
 # Display the data in the app
-st.write("Top Post Titles:")
-st.write(df['data.title'])
+st.dataframe(df)
+print(df.head(1))
+print(df.columns)
 
-# Create a bar chart of the top 10 posts by score
-st.write("Top 10 Posts by Score:")
-top_scores = df.sort_values(by=['data.score'], ascending=False).head(10)
-bar_chart = alt.Chart(top_scores).mark_bar().encode(
-    x='data.score',
-    y=alt.Y('data.title', sort='-x')
+# Create a bar chart of upvotes by title
+chart = alt.Chart(df).mark_bar().encode(
+    x='data.score:Q',
+    y="data.title:N"
 ).properties(
-    width=600,
+    width=500,
     height=400
 )
-st.altair_chart(bar_chart)
 
-# Create a pie chart of the distribution of post types
-st.write("Distribution of Post Types:")
-post_type_counts = df['data.post_hint'].value_counts()
-pie_chart = alt.Chart(post_type_counts.reset_index()).mark_circle().encode(
-    alt.X('data.post_hint', title='Post Type'),
-    alt.Y('data.index', title='Count')
-).properties(
-    width=600,
-    height=400
-)
-st.altair_chart(pie_chart)
+# Display the chart in the app
+st.altair_chart(chart)
